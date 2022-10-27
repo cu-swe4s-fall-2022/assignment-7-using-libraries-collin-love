39f15d532b8abb5e86e11e7b5c65441d7622817d
Collin Love <collin.love@colorado.edu>
2022-10-27 12:10:12 -0600

    func tests pass locally and not on actions?

--- tests/test_plotter.sh
+++ tests/test_plotter.sh
@@ -6,13 +6,14 @@ set -o # pipefail # fail if prior step failed
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
+python -m unittest test_data_processor.py
+pycodestyle $(git ls-files '*.py')
+
 cd ..
 run file_dimensions python data_processor.py get_file_dimensions 'iris.data'
 assert_exit_code 0
 
 run random_mat python data_processor.py get_random_matrix 2 1
 assert_exit_code 0
-cd tests
 
-python -m unittest test_data_processor.py
-pycodestyle $(git ls-files '*.py')
+
