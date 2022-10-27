8d762d0d3232e0e781b03c600d4035b73943f5ae
Collin Love <collin.love@colorado.edu>
2022-10-27 11:44:00 -0600

    added assert_exit_codes to make sure functions were exiting after finish

--- tests/test_plotter.sh
+++ tests/test_plotter.sh
@@ -6,5 +6,13 @@ set -o # pipefail # fail if prior step failed
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
+cd ..
+run file_dimensions python data_processor.py get_file_dimensions 'iris.data'
+assert_exit_code 0
+
+run random_mat python data_processor.py get_random_matrix 2 1
+assert_exit_code 0
+cd tests
+
 python -m unittest test_data_processor.py
 pycodestyle $(git ls-files '*.py')
