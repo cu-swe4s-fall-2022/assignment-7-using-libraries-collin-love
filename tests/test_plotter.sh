2d599ce14ded9a34f86be9adfd0e77c4ae4e7e7c
Collin Love <collin.love@colorado.edu>
2022-10-27 12:16:25 -0600

    runnin unit and pycode

--- tests/test_plotter.sh
+++ tests/test_plotter.sh
@@ -9,11 +9,11 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 python -m unittest test_data_processor.py
 pycodestyle $(git ls-files '*.py')
 
-cd ..
-run file_dimensions python data_processor.py get_file_dimensions 'iris.data'
-assert_exit_code 0
+# cd ..
+# run file_dimensions python data_processor.py get_file_dimensions 'iris.data'
+# assert_exit_code 0
 
-run random_mat python data_processor.py get_random_matrix 2 1
-assert_exit_code 0
+# run random_mat python data_processor.py get_random_matrix 2 1
+# assert_exit_code 0
 
 
